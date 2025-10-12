0363b94e9d3d936d35152336d9809bcdbe7103a1
arymehta <amehta350@gmail.com>
2025-10-12 14:59:49 +0530

    add: initialise database code

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-import sql from "./db/dbconn.js";
-
+import { initDB, deleteDB } from "./db/initdb.js";
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
+// deleteDB()
+initDB();
 app.get("/", (req, res) => {
   res.send("Welcome to the DBMS OPAC API");
 });
