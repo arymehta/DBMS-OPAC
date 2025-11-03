3470050d0d01d062434c18cc3ab7e8adececbf68
arymehta <amehta350@gmail.com>
2025-11-03 10:06:55 +0530

    add: vercel deploy config

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
+const BACKEND_URL = import.meta.env.BACKEND_URL || 'http://localhost:3000';
 
 export { BACKEND_URL };
